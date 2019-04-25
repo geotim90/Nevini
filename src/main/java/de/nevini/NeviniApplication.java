648a5b7ac3e64b62dd9653276dc17e82d3c335c4
Geotim <tmorschel@gmail.com>
2019-04-25 08:04:37 +0200

    Spring Initializr and JDA dependencies

---
+++ src/main/java/de/nevini/NeviniApplication.java
@@ -0,0 +1,13 @@
+package de.nevini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeviniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeviniApplication.class, args);
+	}
+
+}
