094680d0bba291966d74afd15bca9cd7571b0430
geotim90 <tmorschel@gmail.com>
2019-06-14 09:23:32 +0200

    [4.1.1] Use mockito to fix unit tests

--- src/test/java/de/nevini/bot/resolvers/common/FeedResolverTest.java
+++ src/test/java/de/nevini/bot/resolvers/common/FeedResolverTest.java
@@ -1,7 +1,9 @@
 package de.nevini.bot.resolvers.common;
 
+import de.nevini.bot.command.CommandEvent;
 import de.nevini.bot.scope.Feed;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 
@@ -11,16 +13,20 @@ public class FeedResolverTest {
 
     @Test
     public void findOsuEvents() {
+        // mock event
+        CommandEvent event = Mockito.mock(CommandEvent.class);
+        Mockito.when(event.isBotOwner()).thenReturn(false);
+        // set expected result
         ArrayList<Feed> expected = new ArrayList<>(1);
         expected.add(Feed.OSU_EVENTS);
         // needs to match the type (case-insensitive)
-        assertEquals(expected, Resolvers.FEED.findSorted(null, "osu.events"));
+        assertEquals(expected, Resolvers.FEED.findSorted(event, "osu.events"));
         // needs to match the name (case-insensitive)
-        assertEquals(expected, Resolvers.FEED.findSorted(null, "osu! events"));
+        assertEquals(expected, Resolvers.FEED.findSorted(event, "osu! events"));
         // needs to match the enum const name (case-insensitive)
-        assertEquals(expected, Resolvers.FEED.findSorted(null, "osu_events"));
+        assertEquals(expected, Resolvers.FEED.findSorted(event, "osu_events"));
         // needs to match the enum const name without underscores (case-insensitive)
-        assertEquals(expected, Resolvers.FEED.findSorted(null, "osu events"));
+        assertEquals(expected, Resolvers.FEED.findSorted(event, "osu events"));
     }
 
 }
