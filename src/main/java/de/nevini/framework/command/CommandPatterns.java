geotim90/Nevini

commit e048a35d3d242f2607e11ba7c020ee773fc7dd09

[4.3.2] Added support for relative input for `--time` options

src/main/java/de/nevini/framework/command/CommandPatterns.java
@@ -11,7 +11,7 @@ public class CommandPatterns {
      * To validate command keywords, please also check that the keyword does not match {@link #OPTION}.
      * This regular expression pattern does not contain any groups apart from the full match.
      */
-    public static final String KEYWORD = "[a-z0-9+\\-!]{1,32}";
+    public static final String KEYWORD = "[a-z][a-z0-9+\\-!]{0,31}";
 
     /**
      * A regular expression pattern that matches valid options or flags.
@@ -23,13 +23,13 @@ public class CommandPatterns {
      * </ul>
      * Note that this regular expression pattern will match anything prepended with an option flag.
      */
-    public static final String OPTION = "(?:(?:--|//)(\\w+)|[-/](\\w))(\\s+(.+))?";
+    public static final String OPTION = "(?:(?:--|//)([a-z]\\w*)|[-/]([a-z]))(\\s+(.+))?";
 
     /**
      * A regular expression pattern that matches valid mentions for users, members, channels and roles.
      * This regular expression patterns contains one group for the mentioned id.
      * Note that this regular expression pattern will not match mentions prepended with an option flag.
      */
-    public static final String MENTION = "(?<!(?:(?:--|//)\\w+|[-/]\\w)\\s+)<(?:@[!&]?|#)\\d+>";
+    public static final String MENTION = "(?<!(?:(?:--|//)[a-z]\\w*|[-/][a-z])\\s+)<(?:@[!&]?|#)\\d+>";
 
 }

src/main/java/de/nevini/bot/modules/guild/activity/ActivitySetMessageCommand.java
@@ -3,29 +3,15 @@ package de.nevini.bot.modules.guild.activity;
 import de.nevini.bot.command.Command;
 import de.nevini.bot.command.CommandDescriptor;
 import de.nevini.bot.command.CommandEvent;
-import de.nevini.bot.resolvers.StringResolver;
 import de.nevini.bot.resolvers.common.Resolvers;
 import de.nevini.bot.scope.Node;
-import de.nevini.bot.util.Formatter;
 import de.nevini.framework.command.CommandOptionDescriptor;
 import de.nevini.framework.command.CommandReaction;
 import net.dv8tion.jda.core.entities.Member;
-import org.apache.commons.lang3.StringUtils;
 
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.time.format.DateTimeParseException;
 
 class ActivitySetMessageCommand extends Command {
-
-    private static final StringResolver timestampResolver = new StringResolver("timestamp", "time", "t",
-            CommandOptionDescriptor.builder()
-                    .syntax("--time <timestamp>")
-                    .description("A valid ISO 8601 UTC timestamp (e.g. `" + LocalDateTime.now().toString() + "`)."
-                            + "\n`now` can be used as a shortcut for the current date and time.")
-                    .keyword("--time")
-                    .aliases(new String[]{"//time", "-t", "/t"})
-                    .build());
 
     ActivitySetMessageCommand() {
         super(CommandDescriptor.builder()
@@ -36,7 +22,7 @@ class ActivitySetMessageCommand extends Command {
                         + "in this Discord server")
                 .options(new CommandOptionDescriptor[]{
                         Resolvers.MEMBER.describe(false, true),
-                        timestampResolver.describe()
+                        Resolvers.TIMESTAMP.describe()
                 })
                 .details("Note that timestamps provided via this command do not override *real* activity information. "
                         + "Instead, they just provided a manual minimum value for activity reports on this server.")
@@ -49,33 +35,19 @@ class ActivitySetMessageCommand extends Command {
     }
 
     private void acceptMember(CommandEvent event, Member member) {
-        timestampResolver.resolveOptionOrInput(event,
+        Resolvers.TIMESTAMP.resolveOptionOrInput(event,
                 timestamp -> acceptMemberAndTimestamp(event, member, timestamp));
     }
 
-    private void acceptMemberAndTimestamp(CommandEvent event, Member member, String timestamp) {
+    private void acceptMemberAndTimestamp(CommandEvent event, Member member, OffsetDateTime timestamp) {
         // validate timestamp
-        OffsetDateTime dateTime;
-        if (StringUtils.isEmpty(timestamp)) {
-            // command was aborted or no input provided
-            event.reply(CommandReaction.DEFAULT_NOK, event::complete);
+        if (timestamp.isAfter(OffsetDateTime.now())) {
+            event.reply(CommandReaction.WARNING, "You cannot use a timestamp in future!", event::complete);
             return;
-        } else {
-            try {
-                // try to parse input
-                dateTime = Formatter.parseTimestamp(timestamp);
-            } catch (DateTimeParseException e) {
-                // failed to parse timestamp
-                event.reply(CommandReaction.WARNING, "You did not provide a valid timestamp!", event::complete);
-                return;
-            }
-            if (dateTime.isAfter(OffsetDateTime.now())) {
-                event.reply(CommandReaction.WARNING, "You cannot use a timestamp in future!", event::complete);
-                return;
-            }
         }
+
         // set activity
-        event.getActivityService().manualActivityMessage(member, dateTime);
+        event.getActivityService().manualActivityMessage(member, timestamp);
         event.reply(CommandReaction.OK, event::complete);
     }
 

src/main/java/de/nevini/bot/modules/guild/activity/ActivitySetOnlineCommand.java
@@ -3,29 +3,15 @@ package de.nevini.bot.modules.guild.activity;
 import de.nevini.bot.command.Command;
 import de.nevini.bot.command.CommandDescriptor;
 import de.nevini.bot.command.CommandEvent;
-import de.nevini.bot.resolvers.StringResolver;
 import de.nevini.bot.resolvers.common.Resolvers;
 import de.nevini.bot.scope.Node;
-import de.nevini.bot.util.Formatter;
 import de.nevini.framework.command.CommandOptionDescriptor;
 import de.nevini.framework.command.CommandReaction;
 import net.dv8tion.jda.core.entities.Member;
-import org.apache.commons.lang3.StringUtils;
 
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.time.format.DateTimeParseException;
 
 class ActivitySetOnlineCommand extends Command {
-
-    private static final StringResolver timestampResolver = new StringResolver("timestamp", "time", "t",
-            CommandOptionDescriptor.builder()
-                    .syntax("--time <timestamp>")
-                    .description("A valid ISO 8601 UTC timestamp (e.g. `" + LocalDateTime.now().toString() + "`)."
-                            + "\n`now` can be used as a shortcut for the current date and time.")
-                    .keyword("--time")
-                    .aliases(new String[]{"//time", "-t", "/t"})
-                    .build());
 
     ActivitySetOnlineCommand() {
         super(CommandDescriptor.builder()
@@ -35,7 +21,7 @@ class ActivitySetOnlineCommand extends Command {
                 .description("configures user activity information for when they were last online on Discord")
                 .options(new CommandOptionDescriptor[]{
                         Resolvers.MEMBER.describe(false, true),
-                        timestampResolver.describe()
+                        Resolvers.TIMESTAMP.describe()
                 })
                 .details("Note that timestamps provided via this command do not override *real* activity information. "
                         + "Instead, they just provided a manual minimum value for activity reports on this server.")
@@ -48,33 +34,19 @@ class ActivitySetOnlineCommand extends Command {
     }
 
     private void acceptMember(CommandEvent event, Member member) {
-        timestampResolver.resolveOptionOrInput(event,
+        Resolvers.TIMESTAMP.resolveOptionOrInput(event,
                 timestamp -> acceptMemberAndTimestamp(event, member, timestamp));
     }
 
-    private void acceptMemberAndTimestamp(CommandEvent event, Member member, String timestamp) {
+    private void acceptMemberAndTimestamp(CommandEvent event, Member member, OffsetDateTime timestamp) {
         // validate timestamp
-        OffsetDateTime dateTime;
-        if (StringUtils.isEmpty(timestamp)) {
-            // command was aborted or no input provided
-            event.reply(CommandReaction.DEFAULT_NOK, event::complete);
+        if (timestamp.isAfter(OffsetDateTime.now())) {
+            event.reply(CommandReaction.WARNING, "You cannot use a timestamp in future!", event::complete);
             return;
-        } else {
-            try {
-                // try to parse input
-                dateTime = Formatter.parseTimestamp(timestamp);
-            } catch (DateTimeParseException e) {
-                // failed to parse timestamp
-                event.reply(CommandReaction.WARNING, "You did not provide a valid timestamp!", event::complete);
-                return;
-            }
-            if (dateTime.isAfter(OffsetDateTime.now())) {
-                event.reply(CommandReaction.WARNING, "You cannot use a timestamp in future!", event::complete);
-                return;
-            }
         }
+
         // set activity
-        event.getActivityService().manualActivityOnline(member, dateTime);
+        event.getActivityService().manualActivityOnline(member, timestamp);
         event.reply(CommandReaction.OK, event::complete);
     }
 

src/main/java/de/nevini/bot/modules/guild/activity/ActivitySetPlayingCommand.java
@@ -4,29 +4,15 @@ import de.nevini.bot.command.Command;
 import de.nevini.bot.command.CommandDescriptor;
 import de.nevini.bot.command.CommandEvent;
 import de.nevini.bot.db.game.GameData;
-import de.nevini.bot.resolvers.StringResolver;
 import de.nevini.bot.resolvers.common.Resolvers;
 import de.nevini.bot.scope.Node;
-import de.nevini.bot.util.Formatter;
 import de.nevini.framework.command.CommandOptionDescriptor;
 import de.nevini.framework.command.CommandReaction;
 import net.dv8tion.jda.core.entities.Member;
-import org.apache.commons.lang3.StringUtils;
 
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.time.format.DateTimeParseException;
 
 class ActivitySetPlayingCommand extends Command {
-
-    private static final StringResolver timestampResolver = new StringResolver("timestamp", "time", "t",
-            CommandOptionDescriptor.builder()
-                    .syntax("--time <timestamp>")
-                    .description("A valid ISO 8601 UTC timestamp (e.g. `" + LocalDateTime.now().toString() + "`)."
-                            + "\n`now` can be used as a shortcut for the current date and time.")
-                    .keyword("--time")
-                    .aliases(new String[]{"//time", "-t", "/t"})
-                    .build());
 
     ActivitySetPlayingCommand() {
         super(CommandDescriptor.builder()
@@ -37,7 +23,7 @@ class ActivitySetPlayingCommand extends Command {
                 .options(new CommandOptionDescriptor[]{
                         Resolvers.MEMBER.describe(false, true),
                         Resolvers.GAME.describe(),
-                        timestampResolver.describe()
+                        Resolvers.TIMESTAMP.describe()
                 })
                 .details("Note that timestamps provided via this command do not override *real* activity information. "
                         + "Instead, they just provided a manual minimum value for activity reports on this server.")
@@ -54,33 +40,19 @@ class ActivitySetPlayingCommand extends Command {
     }
 
     private void acceptMemberGame(CommandEvent event, Member member, GameData game) {
-        timestampResolver.resolveOptionOrInput(event,
+        Resolvers.TIMESTAMP.resolveOptionOrInput(event,
                 timestamp -> acceptMemberGameTimestamp(event, member, game, timestamp));
     }
 
-    private void acceptMemberGameTimestamp(CommandEvent event, Member member, GameData game, String timestamp) {
+    private void acceptMemberGameTimestamp(CommandEvent event, Member member, GameData game, OffsetDateTime timestamp) {
         // validate timestamp
-        OffsetDateTime dateTime;
-        if (StringUtils.isEmpty(timestamp)) {
-            // command was aborted or no input provided
-            event.reply(CommandReaction.DEFAULT_NOK, event::complete);
+        if (timestamp.isAfter(OffsetDateTime.now())) {
+            event.reply(CommandReaction.WARNING, "You cannot use a timestamp in future!", event::complete);
             return;
-        } else {
-            try {
-                // try to parse input
-                dateTime = Formatter.parseTimestamp(timestamp);
-            } catch (DateTimeParseException e) {
-                // failed to parse timestamp
-                event.reply(CommandReaction.WARNING, "You did not provide a valid timestamp!", event::complete);
-                return;
-            }
-            if (dateTime.isAfter(OffsetDateTime.now())) {
-                event.reply(CommandReaction.WARNING, "You cannot use a timestamp in future!", event::complete);
-                return;
-            }
         }
+
         // set activity
-        event.getActivityService().manualActivityPlaying(member, game, dateTime);
+        event.getActivityService().manualActivityPlaying(member, game, timestamp);
         event.reply(CommandReaction.OK, event::complete);
     }
 

src/main/java/de/nevini/bot/resolvers/common/Resolvers.java
@@ -11,5 +11,6 @@ public class Resolvers {
     public static final NodeResolver NODE = new NodeResolver();
     public static final PermissionResolver PERMISSION = new PermissionResolver();
     public static final RoleResolver ROLE = new RoleResolver();
+    public static final TimestampResolver TIMESTAMP = new TimestampResolver();
 
 }

src/main/java/de/nevini/bot/resolvers/common/TimestampResolver.java
@@ -1,0 +1,106 @@
+package de.nevini.bot.resolvers.common;
+
+import de.nevini.bot.command.CommandEvent;
+import de.nevini.bot.resolvers.AbstractResolver;
+import de.nevini.bot.util.Formatter;
+import de.nevini.framework.command.CommandOptionDescriptor;
+import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeParseException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TimestampResolver extends AbstractResolver<OffsetDateTime> {
+
+    TimestampResolver() {
+        super("timestamp", new Pattern[]{Pattern.compile("(?i)(?:(?:--|//)time(?:stamp)|[-/]t)(?:\\s+(.+))?")});
+    }
+
+    @Override
+    public CommandOptionDescriptor describe(boolean list, boolean argument) {
+        return CommandOptionDescriptor.builder()
+                .syntax(argument ? "[--time] <timestamp>" : "--time <timestamp>")
+                .description("A valid ISO 8601 UTC timestamp (e.g. `" + LocalDateTime.now().toString() + "`)."
+                        + "\n`now` can be used as a shortcut for the current date and time."
+                        + "\nRelative input such as `5 days ago` or `-24h` can also be used."
+                        + (argument ? "\nThe `--time` flag is optional if this option is provided first." : ""))
+                .keyword("--time")
+                .aliases(new String[]{"//time", "--timestamp", "//timestamp", "-t", "/t"})
+                .build();
+    }
+
+    @Override
+    public List<OffsetDateTime> findSorted(CommandEvent ignore, String query) {
+        return Optional.ofNullable(parse(query)).map(Collections::singletonList).orElse(Collections.emptyList());
+    }
+
+    private OffsetDateTime parse(String s) {
+        if (StringUtils.isEmpty(s)) {
+            return null;
+        }
+
+        if (s.matches("\\d{4}-\\d{2}-\\d{2}.*")) {
+            String padding = "1970-01-01T00:00:00";
+            String padded = s + padding.substring(Math.min(padding.length(), s.length()));
+            try {
+                return LocalDateTime.parse(padded).atOffset(ZoneOffset.UTC);
+            } catch (DateTimeParseException ignore) {
+                return null;
+            }
+        }
+
+        switch (s.toLowerCase()) {
+            case "now":
+            case "just now":
+            case "today":
+                return OffsetDateTime.now(ZoneOffset.UTC);
+            case "yesterday":
+                return OffsetDateTime.now(ZoneOffset.UTC).minusDays(1);
+            case "tomorrow":
+                return OffsetDateTime.now(ZoneOffset.UTC).plusDays(1);
+            default:
+                break; // ignore
+        }
+
+        Matcher matcher = Pattern.compile("(\\d+) ?d(ays?)? ago").matcher(s);
+        if (matcher.matches()) {
+            return OffsetDateTime.now(ZoneOffset.UTC).minusDays(Long.parseLong(matcher.group(1)));
+        }
+
+        matcher = Pattern.compile("(-?\\d+) ?d(ays?)?").matcher(s);
+        if (matcher.matches()) {
+            return OffsetDateTime.now(ZoneOffset.UTC).plusDays(Long.parseLong(matcher.group(1)));
+        }
+
+        matcher = Pattern.compile("(\\d+) ?h(ours?)? ago").matcher(s);
+        if (matcher.matches()) {
+            return OffsetDateTime.now(ZoneOffset.UTC).minusHours(Long.parseLong(matcher.group(1)));
+        }
+
+        matcher = Pattern.compile("(-?\\d+) ?h(ours?)?").matcher(s);
+        if (matcher.matches()) {
+            return OffsetDateTime.now(ZoneOffset.UTC).plusHours(Long.parseLong(matcher.group(1)));
+        }
+
+        // no match
+        return null;
+    }
+
+    @Override
+    protected @NonNull String getFieldNameForPicker(OffsetDateTime item) {
+        return Formatter.formatTimestamp(item);
+    }
+
+    @Override
+    protected @NonNull String getFieldValueForPicker(OffsetDateTime item) {
+        return Long.toString(item.toInstant().toEpochMilli());
+    }
+
+}

src/test/java/de/nevini/bot/resolvers/common/TimestampResolverTest.java
@@ -1,0 +1,62 @@
+package de.nevini.bot.resolvers.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+public class TimestampResolverTest {
+
+    @Test
+    public void testDate() {
+        Assert.assertEquals(
+                OffsetDateTime.of(2019, 7, 5, 0, 0, 0, 0, ZoneOffset.UTC),
+                Resolvers.TIMESTAMP.findSorted(null, "2019-07-05").get(0)
+        );
+    }
+
+    @Test
+    public void testDateTime() {
+        Assert.assertEquals(
+                OffsetDateTime.of(2019, 7, 5, 23, 59, 59, 0, ZoneOffset.UTC),
+                Resolvers.TIMESTAMP.findSorted(null, "2019-07-05T23:59:59").get(0)
+        );
+    }
+
+    @Test
+    public void testDateTimeMillis() {
+        Assert.assertEquals(
+                OffsetDateTime.of(2019, 7, 5, 23, 59, 59, 999000000, ZoneOffset.UTC),
+                Resolvers.TIMESTAMP.findSorted(null, "2019-07-05T23:59:59.999").get(0)
+        );
+    }
+
+    @Test
+    public void testNow() {
+        OffsetDateTime before = OffsetDateTime.now(ZoneOffset.UTC);
+        OffsetDateTime timestamp = Resolvers.TIMESTAMP.findSorted(null, "now").get(0);
+        OffsetDateTime after = OffsetDateTime.now(ZoneOffset.UTC);
+        Assert.assertFalse(before.isAfter(timestamp));
+        Assert.assertFalse(after.isBefore(timestamp));
+    }
+
+    @Test
+    public void test5DaysAgo() {
+        OffsetDateTime before = OffsetDateTime.now(ZoneOffset.UTC).minusDays(5);
+        OffsetDateTime timestamp = Resolvers.TIMESTAMP.findSorted(null, "5 days ago").get(0);
+        OffsetDateTime after = OffsetDateTime.now(ZoneOffset.UTC).minusDays(5);
+        Assert.assertFalse(before.isAfter(timestamp));
+        Assert.assertFalse(after.isBefore(timestamp));
+    }
+
+    @Test
+    public void testMinus24H() {
+        OffsetDateTime before = OffsetDateTime.now(ZoneOffset.UTC).minusHours(24);
+        OffsetDateTime timestamp = Resolvers.TIMESTAMP.findSorted(null, "-24h").get(0);
+        OffsetDateTime after = OffsetDateTime.now(ZoneOffset.UTC).minusHours(24);
+        Assert.assertFalse(before.isAfter(timestamp));
+        Assert.assertFalse(after.isBefore(timestamp));
+    }
+
+}

commit 7c117331acc0bec9b7e76b4e744657e58c3bc118

[4.3.2] Fixed command patterns that broke in e048a35d3d242f2607e11ba7c020ee773fc7dd09

src/main/java/de/nevini/framework/command/CommandPatterns.java
@@ -11,7 +11,7 @@ public class CommandPatterns {
      * To validate command keywords, please also check that the keyword does not match {@link #OPTION}.
      * This regular expression pattern does not contain any groups apart from the full match.
      */
-    public static final String KEYWORD = "[a-z][a-z0-9+\\-!]{0,31}";
+    public static final String KEYWORD = "[a-z0-9+\\-!]{1,32}";
 
     /**
      * A regular expression pattern that matches valid options or flags.
@@ -23,13 +23,13 @@ public class CommandPatterns {
      * </ul>
      * Note that this regular expression pattern will match anything prepended with an option flag.
      */
-    public static final String OPTION = "(?:(?:--|//)([a-z]\\w*)|[-/]([a-z]))(\\s+(.+))?";
+    public static final String OPTION = "(?:(?:--|//)(\\w+)|[-/]([a-z]))(\\s+(.+))?";
 
     /**
      * A regular expression pattern that matches valid mentions for users, members, channels and roles.
      * This regular expression patterns contains one group for the mentioned id.
      * Note that this regular expression pattern will not match mentions prepended with an option flag.
      */
-    public static final String MENTION = "(?<!(?:(?:--|//)[a-z]\\w*|[-/][a-z])\\s+)<(?:@[!&]?|#)\\d+>";
+    public static final String MENTION = "(?<!(?:(?:--|//)\\w+|[-/][a-z])\\s+)<(?:@[!&]?|#)\\d+>";
 
 }
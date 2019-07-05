7c117331acc0bec9b7e76b4e744657e58c3bc118
geotim90 <tmorschel@gmail.com>
2019-07-05 09:42:51 +0200

    [4.3.2] Fixed command patterns that broke in e048a35d3d242f2607e11ba7c020ee773fc7dd09

--- src/main/java/de/nevini/framework/command/CommandPatterns.java
+++ src/main/java/de/nevini/framework/command/CommandPatterns.java
@@ -11,7 +11,7 @@ public class CommandPatterns {
      * To validate command keywords, please also check that the keyword does not match {@link #OPTION}.
      * This regular expression pattern does not contain any groups apart from the full match.
      */
-    public static final String KEYWORD = "[a-z][a-z0-9+\\-!]{0,31}";
+    public static final String KEYWORD = "[a-z0-9+\\-!]{1,32}";
 
     /**
      * A regular expression pattern that matches valid options or flags.
@@ -23,13 +23,13 @@ public class CommandPatterns {
      * </ul>
      * Note that this regular expression pattern will match anything prepended with an option flag.
      */
-    public static final String OPTION = "(?:(?:--|//)([a-z]\\w*)|[-/]([a-z]))(\\s+(.+))?";
+    public static final String OPTION = "(?:(?:--|//)(\\w+)|[-/]([a-z]))(\\s+(.+))?";
 
     /**
      * A regular expression pattern that matches valid mentions for users, members, channels and roles.
      * This regular expression patterns contains one group for the mentioned id.
      * Note that this regular expression pattern will not match mentions prepended with an option flag.
      */
-    public static final String MENTION = "(?<!(?:(?:--|//)[a-z]\\w*|[-/][a-z])\\s+)<(?:@[!&]?|#)\\d+>";
+    public static final String MENTION = "(?<!(?:(?:--|//)\\w+|[-/][a-z])\\s+)<(?:@[!&]?|#)\\d+>";
 
 }
