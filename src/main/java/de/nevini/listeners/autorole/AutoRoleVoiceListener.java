8ea9cde682e4525eb9c698a6a01ad8d4b0a1b32a
geotim90 <tmorschel@gmail.com>
2019-07-31 18:07:21 +0200

    [4.4.5] Listen for `GuildVoiceMoveEvent` too

--- src/main/java/de/nevini/listeners/autorole/AutoRoleVoiceListener.java
+++ src/main/java/de/nevini/listeners/autorole/AutoRoleVoiceListener.java
@@ -4,9 +4,12 @@ import de.nevini.services.common.AutoRoleService;
 import de.nevini.util.concurrent.EventDispatcher;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.entities.VoiceChannel;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceMoveEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,38 +24,46 @@ public class AutoRoleVoiceListener {
             @Autowired EventDispatcher eventDispatcher
     ) {
         this.autoRoleService = autoRoleService;
-        eventDispatcher.subscribe(GuildVoiceJoinEvent.class, this::onVoiceJoin);
-        eventDispatcher.subscribe(GuildVoiceLeaveEvent.class, this::onVoiceLeave);
+        eventDispatcher.subscribe(GuildVoiceJoinEvent.class, e -> onVoiceJoin(e.getMember(), e.getChannelJoined()));
+        eventDispatcher.subscribe(GuildVoiceLeaveEvent.class, e -> onVoiceLeave(e.getMember(), e.getChannelLeft()));
+        eventDispatcher.subscribe(GuildVoiceMoveEvent.class, e -> {
+            onVoiceLeave(e.getMember(), e.getChannelLeft());
+            onVoiceJoin(e.getMember(), e.getChannelJoined());
+        });
     }
 
-    private void onVoiceJoin(GuildVoiceJoinEvent e) {
+    private void onVoiceJoin(Member member, VoiceChannel channel) {
         // get role
-        Role role = autoRoleService.getVoiceRole(e.getChannelJoined());
+        Role role = autoRoleService.getVoiceRole(channel);
         if (role == null) return;
 
         // check permissions
-        if (!e.getGuild().getSelfMember().hasPermission(Permission.MANAGE_ROLES)) return;
+        if (!member.getGuild().getSelfMember().hasPermission(Permission.MANAGE_ROLES)) return;
 
         // add role if not present
-        if (!e.getMember().getRoles().contains(role)) {
-            log.debug("Adding role {} to {}", role, e.getMember());
-            e.getGuild().getController().addSingleRoleToMember(e.getMember(), role).queue();
+        if (!member.getRoles().contains(role)) {
+            log.debug("Adding role {} to {}", role, member);
+            member.getGuild().getController().addSingleRoleToMember(member, role).queue();
         }
     }
 
-    private void onVoiceLeave(GuildVoiceLeaveEvent e) {
+    private void onVoiceLeave(Member member, VoiceChannel channel) {
         // get role
-        Role role = autoRoleService.getVoiceRole(e.getChannelLeft());
+        Role role = autoRoleService.getVoiceRole(channel);
         if (role == null) return;
 
         // check permissions
-        if (!e.getGuild().getSelfMember().hasPermission(Permission.MANAGE_ROLES)) return;
+        if (!member.getGuild().getSelfMember().hasPermission(Permission.MANAGE_ROLES)) return;
 
         // remove role if present
-        if (e.getMember().getRoles().contains(role)) {
-            log.debug("Removing role {} from {}", role, e.getMember());
-            e.getGuild().getController().removeSingleRoleFromMember(e.getMember(), role).queue();
+        if (member.getRoles().contains(role)) {
+            log.debug("Removing role {} from {}", role, member);
+            member.getGuild().getController().removeSingleRoleFromMember(member, role).queue();
         }
+    }
+
+    private void onVoiceMove(GuildVoiceMoveEvent e) {
+
     }
 
 }
