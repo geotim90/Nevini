e3c3d9f0b231b6a7bcda7db1a3d6dd11e17a1c74
geotim90 <tmorschel@gmail.com>
2019-08-07 11:33:18 +0200

    [4.5.1] Fixed unit tests

--- src/test/java/de/nevini/resolvers/common/ModuleResolverTest.java
+++ src/test/java/de/nevini/resolvers/common/ModuleResolverTest.java
@@ -1,7 +1,9 @@
 package de.nevini.resolvers.common;
 
+import de.nevini.command.CommandEvent;
 import de.nevini.scope.Module;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 
@@ -13,8 +15,10 @@ public class ModuleResolverTest {
     public void findCore() {
         ArrayList<Module> expected = new ArrayList<>(1);
         expected.add(Module.CORE);
+        // mock event
+        CommandEvent event = Mockito.mock(CommandEvent.class);
         // needs to match the name (case-insensitive)
-        assertEquals(expected, Resolvers.MODULE.findSorted(null, "core"));
+        assertEquals(expected, Resolvers.MODULE.findSorted(event, "core"));
     }
 
 }
--- src/test/java/de/nevini/resolvers/osu/OsuModsResolverTest.java
+++ src/test/java/de/nevini/resolvers/osu/OsuModsResolverTest.java
@@ -1,7 +1,9 @@
 package de.nevini.resolvers.osu;
 
 import de.nevini.api.osu.model.OsuMod;
+import de.nevini.command.CommandEvent;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertArrayEquals;
 
@@ -12,16 +14,18 @@ public class OsuModsResolverTest {
     @Test
     public void findHardRock() {
         OsuMod[] expected = {OsuMod.HARD_ROCK};
+        // mock event
+        CommandEvent event = Mockito.mock(CommandEvent.class);
         // needs to match the name (case-insensitive)
-        assertArrayEquals(expected, resolver.findSorted(null, "hard rock").get(0));
+        assertArrayEquals(expected, resolver.findSorted(event, "hard rock").get(0));
         // needs to match the name (case-insensitive substring)
-        assertArrayEquals(expected, resolver.findSorted(null, "rock").get(0));
+        assertArrayEquals(expected, resolver.findSorted(event, "rock").get(0));
         // needs to match the enum const name (case-insensitive)
-        assertArrayEquals(expected, resolver.findSorted(null, "hard_rock").get(0));
+        assertArrayEquals(expected, resolver.findSorted(event, "hard_rock").get(0));
         // needs to match the enum const name without underscores (case-insensitive)
-        assertArrayEquals(expected, resolver.findSorted(null, "hard rock").get(0));
+        assertArrayEquals(expected, resolver.findSorted(event, "hard rock").get(0));
         // needs to match the short code (case-insensitive)
-        assertArrayEquals(expected, resolver.findSorted(null, "HR").get(0));
+        assertArrayEquals(expected, resolver.findSorted(event, "HR").get(0));
     }
 
 }
