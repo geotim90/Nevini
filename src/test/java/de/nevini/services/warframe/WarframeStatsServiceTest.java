5c5388084b940f5257e5ea493b2d90eaf7f976df
geotim90 <tmorschel@gmail.com>
2020-08-27 11:12:59 +0200

    [5.1.0] build fix

--- src/test/java/de/nevini/services/warframe/WarframeStatsServiceTest.java
+++ src/test/java/de/nevini/services/warframe/WarframeStatsServiceTest.java
@@ -1,14 +1,22 @@
 package de.nevini.services.warframe;
 
 import de.nevini.api.wfs.model.weapons.WfsWeapon;
+import de.nevini.data.wfs.*;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class WarframeStatsServiceTest {
 
+    private final WarframeStatusApiProvider apiProvider = new WarframeStatusApiProvider();
+
     @Test
     public void testRepairWeapons() {
-        WarframeStatusService service = new WarframeStatusService();
+        WarframeStatusService service = new WarframeStatusService(
+                new WfsDropDataService(apiProvider),
+                new WfsRivenDataService(apiProvider),
+                new WfsWeaponDataService(apiProvider),
+                new WfsWorldStateDataService(apiProvider)
+        );
         for (WfsWeapon weapon : service.getWeapons()) {
             Assert.assertNotNull(weapon.getWikiaThumbnail());
             Assert.assertNotNull(weapon.getWikiaUrl());
